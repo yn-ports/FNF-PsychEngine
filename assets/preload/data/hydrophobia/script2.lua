6e87b835ebcbabb165f0fcd889099d693ae22d96
pwp114514 <117086223+pwp114514@users.noreply.github.com>
2023-01-30 17:43:03 +0800

    Add files via upload

---
+++ assets/preload/data/hydrophobia/script2.lua
@@ -0,0 +1,14 @@
+function onStepHit()
+    if curBeat > 64 then
+    triggerEvent('Add Camera Zoom', '0.05','0')
+    if curBeat > 320 then
+    triggerEvent('Add Camera Zoom', '0','0')
+    if curBeat > 352 then
+    triggerEvent('Add Camera Zoom', '0.05','0')
+    if curBeat > 482 then
+    triggerEvent('Add Camera Zoom', '0','0')
+                end
+            end
+        end
+    end
+end
