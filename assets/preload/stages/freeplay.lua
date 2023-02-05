82810f0ba086770e0fb4f43c53a6507f1146f0ff
fnf small porter <117057486+Liulw123@users.noreply.github.com>
2023-02-05 11:36:36 +0800

    Add files via upload

--- assets/preload/stages/freeplay.lua
+++ assets/preload/stages/freeplay.lua
@@ -17,6 +17,14 @@ function onCreate()
 	makeLuaSprite('info2', 'freeplay/infoDifficulty', -600, 0)
 	addLuaSprite('info2')
 	setScrollFactor('info2', 1, 1);
+	
+	makeLuaText("tips", 'Left = Start song\nUp = Last song\nDown = Next song\nRight = Exit to freeplay', 1000, 250, 600)
+	setTextSize('tips', 25)
+	setObjectCamera('tips', 'other')
+	addLuaText("tips")
+	setTextAlignment('tips', 'right')
+	
+	setProperty('skipCountdown', true)
 end
 
 function onCreatePost()
@@ -33,4 +41,9 @@ end
 
 function onUpdate()
          triggerEvent('Add Camera Zoom','0','1')
+         playMusic('freakyMenu', 0.8)
+end
+
+function onStartCountdown()
+	playMusic('freakyMenu', 0.8)
 end
