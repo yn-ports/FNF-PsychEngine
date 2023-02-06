7cd04a57b3a3a5c80dfddcfb560cce653043b638
fnf small porter <117057486+Liulw123@users.noreply.github.com>
2023-02-06 13:21:18 +0800

    fix stage lua error lol

--- assets/preload/stages/bluebg2.lua
+++ assets/preload/stages/bluebg2.lua
@@ -1,11 +1,6 @@
 function onCreate()
 	-- background shit
 
-	makeLuaSprite('blue2', 'blue2', -500, -10);
-	setLuaSpriteScrollFactor('blue2', 1, 1);
-	scaleObject('blue2', 1, 1);
-                addLuaSprite('blue2',false)
-
     makeAnimatedLuaSprite('NewRAINLayer01','NewRAINLayer01',-400,-200)
     scaleObject('NewRAINLayer01', 2.5, 2.5);
     addAnimationByPrefix('NewRAINLayer01','RainFirstlayer instance','RainFirstlayer instance',24,true)
@@ -16,12 +11,18 @@ function onCreate()
     addAnimationByPrefix('bg','bg','bg',24,true)
   addLuaSprite('bg',false)
   setProperty('bg.visible',false)
+  
+  makeLuaSprite('blue2', 'blue2', -500, -10);
+	setLuaSpriteScrollFactor('blue2', 1, 1);
+	scaleObject('blue2', 1, 1);
+    addLuaSprite('blue2',false)
 
 end
 
 function onStepHit()				
 	if curStep == 512 then
 		setProperty('bg.visible',true)
+		setProperty('blue2.visible', false)
 		setProperty('boyfriendGroup.visible',false)
 		if curStep % 2 == 0 then
 			objectPlayAnimation('bg', 'bg');
@@ -29,6 +30,7 @@ function onStepHit()
 	end
 	if curStep == 768 then
 		setProperty('bg.visible',false)
+		setProperty('blue2.visible', true)
 		setProperty('boyfriendGroup.visible',true)
 		removeLuaSprite('bg',true)
 	end
