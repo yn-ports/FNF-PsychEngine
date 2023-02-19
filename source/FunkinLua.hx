1592b7d47ecdd639dcf204079dbb9d95d539e352
fnf small porter <117057486+Liulw123@users.noreply.github.com>
2023-02-19 12:53:37 +0800

    e

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -2203,23 +2203,6 @@ class FunkinLua {
 			return true;
 			#end
 		});
-		Lua_helper.add_callback(lua, "startVideoSprite",
-			function(videoFile:String, x:Float = 0, y:Float = 0, op:Float = 1, cam:String = 'world',
-				?loop:Bool = false, ?pauseMusic:Bool = false) {
-			//
-			#if VIDEOS_ALLOWED
-			if(FileSystem.exists(Paths.video(videoFile))) {
-				PlayState.instance.startVideoSprite(videoFile, x, y, op, cam, loop, pauseMusic);
-				return true;
-			} else {
-				luaTrace('startVideoSprite: Video file not found: ' + videoFile, false, false, FlxColor.RED);
-			}
-			return false;
-			#else
-			PlayState.instance.startAndEnd();
-			return true;
-			#end
-		});
 		
 		Lua_helper.add_callback(lua, "playMusic", function(sound:String, volume:Float = 1, loop:Bool = false) {
 			FlxG.sound.playMusic(Paths.music(sound), volume, loop);
