4de6fe1a764021a4574663d5ba13fbf4232482c7
Liulw123 <117057486+Liulw123@users.noreply.github.com>
2023-03-18 08:11:11 +0800

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1883,7 +1883,7 @@ class PlayState extends MusicBeatState
 		}
 
 		var video:MP4Handler = new MP4Handler();
-		video.playVideo(SUtil.getStorageDirectory() + path, false, false);
+		video.playVideo(SUtil.getStorageDirectory() + name, false, false);
 		video.finishCallback = function()
 		{
 			startAndEnd();
